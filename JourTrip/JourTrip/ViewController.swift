c72c58c4e10576dcef7fa0db172730693288c11f
Alex Cros <alexcroscuadros@gmail.com>
2019-04-23 21:24:03 +0200

    Initial commit

---
+++ JourTrip/JourTrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JourTrip
+//
+//  Created by Alexandre on 23/4/19.
+//  Copyright © 2019 Los Zigerianos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
