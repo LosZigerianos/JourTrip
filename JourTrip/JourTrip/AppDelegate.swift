c94501e290f737bee630bd60b65cc3be9807d9db
Adrián <adri4silva@gmail.com>
2019-05-02 23:34:36 +0200

    [Feature/Login] Add ViewModel dependency

--- JourTrip/JourTrip/AppDelegate.swift
+++ JourTrip/JourTrip/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        let loginViewModel = LoginViewModel()
+        let serviceProxy = ServiceProxy()
+        let loginViewModel = LoginViewModel(loginService: serviceProxy)
         let loginViewController = LoginViewController(viewModel: loginViewModel)
 
         window?.rootViewController = loginViewController
