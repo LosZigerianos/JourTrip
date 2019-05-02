861d51de3c74aa7d88d5164be3322ebc7a61715d
Adrián <adri4silva@gmail.com>
2019-05-02 23:27:54 +0200

    [Feature/Login] Add Rx layer to ServiceProxy

--- JourTrip/JourTrip/Network/ServiceProxy.swift
+++ JourTrip/JourTrip/Network/ServiceProxy.swift
@@ -9,10 +9,31 @@
 import UIKit
 import Alamofire
 import AlamofireObjectMapper
+import RxSwift
 
-class ServiceProxy: NSObject {
-    
-    func register(email: String, password: String, completion:@escaping (_ response:UserResponse?, _ error:NSError?) -> Void) {
+protocol LoginServiceType {
+    func login(email: String,
+               password: String,
+               completion: @escaping (_ response: UserLogin?, _ error: Error?) -> Void)
+    func login(with email: String, password: String) -> Single<Void>
+}
+
+protocol RegisterServiceType {
+    func register(email: String,
+                  password: String,
+                  completion: @escaping (_ response: UserResponse?, _ error: Error?) -> Void)
+}
+
+protocol LocationsServiceType {
+    func getLocations(token: String,
+                      completion: @escaping (_ response: LocationsResponse?, _ error: Error?) -> Void)
+}
+
+class ServiceProxy: LoginServiceType, RegisterServiceType, LocationsServiceType {
+
+    func register(email: String,
+                  password: String,
+                  completion: @escaping (_ response: UserResponse?, _ error: Error?) -> Void) {
         let parameters: Parameters = ["email": email, "password": password]
         
         let url = ConstantNetworking.localUrl + ConstantNetworking.signup
@@ -25,7 +46,9 @@ class ServiceProxy: NSObject {
         }
     }
     
-    func login(email: String, password: String, completion:@escaping (_ response:UserLogin?, _ error:NSError?) -> Void) {
+    func login(email: String,
+               password: String,
+               completion: @escaping (_ response: UserLogin?, _ error: Error?) -> Void) {
         let parameters: Parameters = ["email": email, "password": password]
         
         let url = ConstantNetworking.localUrl + ConstantNetworking.login
@@ -37,8 +60,24 @@ class ServiceProxy: NSObject {
             }
         }
     }
+
+    func login(with email: String, password: String) -> Single<Void> {
+        return Single.create { observer in
+            self.login(email: email, password: password) { response, error in
+                if let error = error {
+                    observer(.error(error))
+                }
+
+                if let _ = response {
+                    observer(.success(()))
+                }
+            }
+            return Disposables.create()
+        }
+    }
     
-    func getLocations(token:String, completion:@escaping (_ response:LocationsResponse?, _ error:NSError?) -> Void) {
+    func getLocations(token: String,
+                      completion: @escaping (_ response: LocationsResponse?, _ error: Error?) -> Void) {
         let url = ConstantNetworking.localUrl + ConstantNetworking.locations + "?name=Ayuntamiento&token=\(token)"
         
         Alamofire.request(url).responseObject { (response: DataResponse<LocationsResponse>) in
