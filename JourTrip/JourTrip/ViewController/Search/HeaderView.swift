0c03d97c1778c41fa89b86bcdb4b9076984e49e3
Alex Cros <alexcroscuadros@gmail.com>
2019-05-28 22:53:09 +0200

    (WIP) Assets refactor

---
+++ JourTrip/JourTrip/ViewController/Search/HeaderView.swift
@@ -0,0 +1,19 @@
+//  HeaderView.swift
+//  JourTrip
+//
+//  Created by Alexandre on 27/05/2019.
+//  Copyright © 2019 Los Zigerianos. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+}
