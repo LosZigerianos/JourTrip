553f7b22d9e225d7810b3519c386054c85b2ba32
Alex Cros <alexcroscuadros@gmail.com>
2019-06-08 22:52:46 +0200

    (WIP) Add Profile custom cells

---
+++ JourTrip/JourTrip/Scenes/Profile/Presentation/ProfileListCell.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileListCell.swift
+//  JourTrip
+//
+//  Created by Alexandre on 08/06/2019.
+//  Copyright © 2019 Los Zigerianos. All rights reserved.
+//
+
+//import UIKit
+//
+//class ProfileListCell: UITableViewCell {
+//    static let reuseIdentifier = String(describing: self)
+//    
+//    @IBOutlet private weak var titleLabel: UILabel!
+//    @IBOutlet private weak var addressDescriptionLabel: UILabel!
+//    @IBOutlet private weak var locationImageView: UIImageView!
+//    @IBOutlet private weak var tagLabel: UILabel!
+//    @IBOutlet private weak var descriptionLabel: UILabel!
+//    @IBOutlet private weak var creationDateLabel: UILabel!
+//    
+//    override func prepareForReuse() {
+//        titleLabel.text = nil
+//        addressDescriptionLabel.text = nil
+//        locationImageView.image = nil
+//        creationDateLabel.text = nil
+//        descriptionLabel.text = nil
+//        
+//        super.prepareForReuse()
+//    }
+//    
+//}
+//
---
+++ JourTrip/JourTrip/Scenes/Profile/Presentation/ProfileTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileTableViewCell.swift
+//  JourTrip
+//
+//  Created by Alexandre on 08/06/2019.
+//  Copyright © 2019 Los Zigerianos. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class ProfileTableViewCell: UITableViewCell {
+    static let reuseIdentifier = String(describing: self)
+    
+    // MARK: - Outlets
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var accountLabel: UILabel!
+    @IBOutlet var userImageView: UIImageView!
+    @IBOutlet var followingValueLabel: UILabel!
+    @IBOutlet var followerValueLabel: UILabel!
+    @IBOutlet var postsValueLabel: UILabel!
+    @IBOutlet var followLabel: UILabel!
+    @IBOutlet var followerLabel: UILabel!
+    @IBOutlet var postsLabel: UILabel!
+
+    func setup(with username: String,
+                    account: String,
+                    imageURL: URL,
+                    following: String,
+                    followers: String,
+                    posts: String
+                    ) {
+        nameLabel.text = username
+        accountLabel.text = account
+        userImageView.kf.setImage(with: imageURL)
+        followingValueLabel.text = following
+        followerValueLabel.text = followers
+        postsValueLabel.text = posts
+    }
+}
