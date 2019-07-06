5ba4a38a2282fbe1e9c12bc84a8baee49062e43f
Alex Cros <alexcroscuadros@gmail.com>
2019-07-06 13:21:16 +0200

    (WIP) vamos

---
+++ JourTrip/JourTrip/Scenes/Profile/Following/DI/FollowingAssembly.swift
@@ -0,0 +1,9 @@
+//
+//  FollowingAssembly.swift
+//  JourTrip
+//
+//  Created by Alexandre on 04/07/2019.
+//  Copyright © 2019 Los Zigerianos. All rights reserved.
+//
+
+import Foundation
---
+++ JourTrip/JourTrip/Scenes/Profile/Following/Presentation/FollowingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FollowingTableViewCell.swift
+//  JourTrip
+//
+//  Created by Alexandre on 04/07/2019.
+//  Copyright © 2019 Los Zigerianos. All rights reserved.
+//
+
+import UIKit
+
+class FollowingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
